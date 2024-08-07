c265a4d32a9f1abd7ffe4f985b5aabe320c8d7ad
Asher <bs.seo@gabia.com>
2024-08-07 17:35:57 +0900

    feat: default input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,53 @@
+import { InputHTMLAttributes } from 'react';
+
+type InputPropsBase = Omit<InputHTMLAttributes<HTMLInputElement>, 'size'>;
+
+interface InputProps extends InputPropsBase {
+  /** Determines whether the input should be styled as primary, `true` by default */
+  primary?: boolean;
+
+  /** Content section rendered on the left side of the input */
+  leftSection?: React.ReactNode;
+
+  /** Props passed down to the `leftSection` element */
+  leftSectionProps?: React.ComponentPropsWithoutRef<'div'>;
+
+  /** Content section rendered on the right side of the input */
+  rightSection?: React.ReactNode;
+
+  /** Props passed down to the `rightSection` element */
+  rightSectionProps?: React.ComponentPropsWithoutRef<'div'>;
+
+  /** Sets `required` attribute on the `input` element */
+  required?: boolean;
+
+  /** Sets `disabled` attribute on the `input` element */
+  disabled?: boolean;
+
+  /** Controls input `height` and horizontal `padding`, `'medium'` by default */
+  size?: 'small' | 'medium' | 'large';
+
+  /** Determines whether the input should have red border and red text color when the `error` prop is set, `true` by default */
+  withErrorStyles?: boolean;
+}
+
+export const Input = ({
+  primary = true,
+  required,
+  disabled,
+  size,
+  leftSection,
+  leftSectionProps,
+  rightSection,
+  rightSectionProps,
+  withErrorStyles,
+  ...rest
+}: InputProps) => {
+  return (
+    <>
+      {leftSection && <div {...leftSectionProps}>{leftSection}</div>}
+      <input {...rest} required={required} disabled={disabled} />
+      {rightSection && <div {...rightSectionProps}>{rightSection}</div>}
+    </>
+  );
+};
---
+++ src/stories/Input.stories.ts
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { Input } from '@/components';
+
+const meta = {
+  title: 'Example/Input',
+  component: Input,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: { onClick: fn() },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    primary: true,
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    primary: false,
+  },
+};
+
+export const Required: Story = {
+  args: {
+    required: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
