e28c9b1cb45a476327233fcf537280dd64016fca
Asher <bs.seo@gabia.com>
2024-08-19 18:05:34 +0900

    feat: text component

---
+++ src/components/Text/Text.css.ts
@@ -0,0 +1,53 @@
+import { recipe } from '@vanilla-extract/recipes';
+
+export const textStyle = recipe({
+  base: {},
+  variants: {
+    inline: {
+      true: {
+        lineHeight: 1,
+      },
+    },
+    weight: {
+      normal: {
+        fontWeight: 'normal',
+      },
+      bold: {
+        fontWeight: 'bold',
+      },
+      lighter: {
+        fontWeight: 'lighter',
+      },
+      bolder: {
+        fontWeight: 'bolder',
+      },
+      100: {
+        fontWeight: '100',
+      },
+      200: {
+        fontWeight: '200',
+      },
+      300: {
+        fontWeight: '300',
+      },
+      400: {
+        fontWeight: '400',
+      },
+      500: {
+        fontWeight: '500',
+      },
+      600: {
+        fontWeight: '600',
+      },
+      700: {
+        fontWeight: '700',
+      },
+      800: {
+        fontWeight: '800',
+      },
+      900: {
+        fontWeight: '900',
+      },
+    },
+  },
+});
---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,48 @@
+import { HTMLAttributes } from 'react';
+import * as styles from './Text.css';
+
+type TextPropsBase = HTMLAttributes<HTMLParagraphElement>;
+
+interface TextProps extends TextPropsBase {
+  /** Sets the font size, `14` by default */
+  fontSize?: number;
+
+  /** Sets the font color */
+  color?: string;
+
+  /** Sets the font weight */
+  weight?: 'normal' | 'bold' | 'lighter' | 'bolder' | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+
+  /** Sets `line-height` to 1 for centering, `false` by default */
+  inline?: boolean;
+
+  /** Shorthand for `component="span"`, `false` by default, default root element is `p` */
+  span?: boolean;
+}
+
+export const Text = ({ fontSize, color, weight, inline, span, children }: TextProps) => {
+  if (span)
+    return (
+      <span className={styles.textStyle({ inline, weight })}>
+        {children}
+        <style jsx>{`
+          span {
+            font-size: ${fontSize};
+            color: ${color};
+          }
+        `}</style>
+      </span>
+    );
+
+  return (
+    <p className={styles.textStyle({ inline, weight })}>
+      {children}
+      <style jsx>{`
+        span {
+          font-size: ${fontSize};
+          color: ${color};
+        }
+      `}</style>
+    </p>
+  );
+};
