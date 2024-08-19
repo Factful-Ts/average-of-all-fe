76e6ff5bca4d0d42a8ab8ee2e7b406ca31e587f8
Asher <bs.seo@gabia.com>
2024-08-19 18:12:37 +0900

    feat: default text story

---
+++ src/stories/Text.stories.tsx
@@ -0,0 +1,22 @@
+import { fn } from '@storybook/test';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Text } from '@/components/Text';
+
+const meta = {
+  title: 'Example/Text',
+  component: Text,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: { onClick: fn() },
+} satisfies Meta<typeof Text>;
+export default meta;
+
+type Story = StoryObj<typeof Text>;
+
+export const Default: Story = {
+  args: {
+    children: 'children',
+  },
+};
