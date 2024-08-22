2692995ed3b16dd95e7a120b35f34963167d5115
Asher <bs.seo@gabia.com>
2024-08-22 16:05:55 +0900

    feat: introduce page text mark up

--- src/containers/Introduce/Introduce.css.ts
+++ src/containers/Introduce/Introduce.css.ts
@@ -1,10 +1,19 @@
 import { style } from '@vanilla-extract/css';
+import { flex } from '@/styles/recipes.css';
 
-export const introduceWrapper = style({
-  width: '100vw',
-  paddingTop: 86,
-});
+export const introduceWrapper = style([
+  flex({ align: 'center', direction: 'column' }),
+  {
+    width: '100vw',
+    paddingTop: 86,
+  },
+]);
 
 export const sloganStyle = style({
   textAlign: 'center',
+});
+
+export const searchBarStyle = style({
+  marginTop: 55,
+  marginBottom: 36,
 });
--- src/containers/Introduce/Introduce.tsx
+++ src/containers/Introduce/Introduce.tsx
@@ -1,10 +1,10 @@
 import * as styles from './Introduce.css';
-import { Text } from '@/components';
+import { SearchBar, Text } from '@/components';
 
-const Title = ({ content, primary }: { content: string; primary?: boolean }) => {
+const Title = ({ primary, children }: { primary?: boolean; children: React.ReactNode }) => {
   return (
     <Text span color={primary ? 'primary' : ''} fontSize={50} weight="bold">
-      {content}
+      {children}
     </Text>
   );
 };
@@ -13,13 +13,31 @@ export const Introduce = () => {
   return (
     <main className={styles.introduceWrapper}>
       <Text className={styles.sloganStyle}>
-        <Title content="세상의 모든 " primary />
-        <Title content="행복을 위한" />
+        <Title primary>세상의 모든 </Title>
+        <Title>행복을 위한</Title>
       </Text>
       <Text className={styles.sloganStyle}>
-        <Title content="평균" primary />
-        <Title content="을 나타냅니다" />
+        <Title primary>평균</Title>
+        <Title>을 나타냅니다</Title>
       </Text>
+      <SearchBar className={styles.searchBarStyle} />
+      <Text fontSize={20} weight="bold">
+        평소 궁금한 데이터가 있으신가요?
+      </Text>
+      <br />
+      <br />
+      <Text>
+        <Text span fontSize={40} weight="bold">
+          필요한 데이터를 맞춤형
+        </Text>
+        <Text span fontSize={40}>
+          으로
+        </Text>
+      </Text>
+      <Text fontSize={40}>더 확실하게 제공해드려요</Text>
+      <br />
+      <Text fontSize={20}>카테고리로 쉽게, AI가 추천해주는 맞춤형 데이터로 더 확실하게</Text>
+      <Text fontSize={20}>정보를 제공해드립니다.</Text>
     </main>
   );
 };
