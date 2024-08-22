af4b9101d5b44aea5549b140714b70cbdfe0f45f
Asher <bs.seo@gabia.com>
2024-08-22 15:53:34 +0900

    chore: add searchbar wrapper

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -4,13 +4,14 @@ import { IconSearch } from '#/icons';
 
 export const SearchBar = ({ className, ...rest }: { className?: string | undefined }) => {
   return (
-    <Input
-      className={className}
-      {...rest}
-      size="large"
-      leftSection={<IconSearch className={styles.searchIconStyle} />}
-      rightSection={<Button className={styles.searchButtonStyle} label="검색하기" size="large" />}
-      hasRightSectionPadding={false}
-    />
+    <div className={className}>
+      <Input
+        {...rest}
+        size="large"
+        leftSection={<IconSearch className={styles.searchIconStyle} />}
+        rightSection={<Button className={styles.searchButtonStyle} label="검색하기" size="large" />}
+        hasRightSectionPadding={false}
+      />
+    </div>
   );
 };
